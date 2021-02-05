0def5c3a3db5218df85bf01e4e87253941c0659b
Akhil Anilkumar <akhil.slipknot@hotmail.com>
2021-02-05 15:57:50 +0530

    Lazy loading for user module is implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MessageComponent } from './message/message.component';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { Page404Component } from "./page-404/page-404.component";
-import { UserModule } from './user.module';
-
 @NgModule({
   imports: [
-    UserModule,
+    BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: "messages",
         component: MessageComponent,
         outlet: 'popup'
+      },
+      {
+        path: 'users',
+        loadChildren: () => import('./user.module').then(m => m.UserModule)
       },
       {
         path: "",
@@ -34,6 +39,6 @@ import { UserModule } from './user.module';
     LandingPageComponent,
   ],
   bootstrap: [AppComponent],
-  
+
 })
 export class AppModule { }
--- src/app/user.module.ts
+++ src/app/user.module.ts
@@ -1,47 +1,41 @@
-import { MessageComponent } from './message/message.component';
-import { LoaderComponent } from './loader/loader.component';
-import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { UserEditComponent } from './user-edit/user-edit.component';
-import { UserResolverService } from './shared/user-resolver.service';
-import { UserDetailGuard } from './shared/user-detail.guard';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { UserListResolverService } from './shared/user-list-resolver.service';
-import { ProductListComponent } from './product-list/product-list.component';
-import { AuthGuard } from './auth.guard';
+import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from "@angular/router";
+import { LoaderComponent } from './loader/loader.component';
+import { MessageComponent } from './message/message.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
+import { UserDetailGuard } from './shared/user-detail.guard';
+import { UserListResolverService } from './shared/user-list-resolver.service';
+import { UserResolverService } from './shared/user-resolver.service';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
     imports: [
-        BrowserModule,
         FormsModule,
-        HttpClientModule,
-        RouterModule.forChild([{
-            path: "users",
-            canActivate: [AuthGuard],
-            children: [
-                {
-                    path: '',
-                    component: ProductListComponent,
-                    resolve: { products: UserListResolverService }
-                }, {
-                    path: ":id",
-                    component: UserDetailsComponent,
-                    canActivate: [UserDetailGuard],
-                    resolve: { user: UserResolverService },
-                    children: [
-                        { path: 'info', component: UserEditComponent }
-                    ],
-                }, {
-                    path: "edit/:id",
-                    component: UserEditComponent
-                }
-            ]
-        }])
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: ProductListComponent,
+                resolve: { products: UserListResolverService }
+            }, {
+                path: ":id",
+                component: UserDetailsComponent,
+                canActivate: [UserDetailGuard],
+                resolve: { user: UserResolverService },
+                children: [
+                    { path: 'info', component: UserEditComponent }
+                ],
+            }, {
+                path: "edit/:id",
+                component: UserEditComponent
+            }
+        ])
     ],
+    exports: [RouterModule],
     declarations: [
         ProductListComponent,
         ConvertToSpacePipe,
@@ -49,7 +43,7 @@ import { RouterModule } from "@angular/router";
         UserDetailsComponent,
         UserEditComponent,
         MessageComponent
-      ],
+    ],
     providers: [UserResolverService, UserListResolverService]
 })
 export class UserModule { }
